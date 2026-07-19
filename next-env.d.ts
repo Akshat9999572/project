baad38d555e386f63c1ff0bbf434a1bd6dea7e8c
Akshat9999572 <akshatshukla2019@gmail.com>
2026-07-19 21:37:00 +0530

    Add ContentPulse dashboard source

---
+++ next-env.d.ts
@@ -0,0 +1,10 @@
+Exit code: 0
+Wall time: 3 seconds
+Output:
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+import "./.next/dev/types/routes.d.ts";
+
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
+
